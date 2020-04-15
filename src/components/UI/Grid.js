9b42516764fe61c970e1b048c09dbe8ed50b471f
Anton <anton@oas.nu>
2020-04-15 14:18:39 +0200

    DEV: Safari fixes

--- src/components/UI/Grid.js
+++ src/components/UI/Grid.js
@@ -36,6 +36,13 @@ const BaseSectionContainer = styled(BaseContainer)`
 
   ${theme.above.md} {
     padding: 90px; 
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 
   ${theme.above.xxl} {
